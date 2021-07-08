a154f76a83a2446fabc1a0665c7d697446ab2d0e
Rudra Kakadia <rkakadia@Rudras-MacBook-Air.local>
2021-07-08 19:58:07 +0530

    constructed basic structure of grid. Imported text from problem.txt and created a grid int array

---
+++ Assignment1/src/Grid.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Grid {
+	
+	// 0 - represents a free space where the agent can move
+	// 1 - represents a block where the agent cannot move
+	// 2 - represents the starting point
+	// 3 - represents the ending point
+	// 4 - represents the free space used in the path
+	private int[][] grid;
+	private int[] start;
+	private int[] goal;
+	private int mazeSize;
+
+	public Grid(File gridFile, int mazeSize, int[] start, int[] goal) throws FileNotFoundException {
+		this.mazeSize = mazeSize;
+		this.start = start;
+		this.goal = goal;
+		buildGrid(gridFile);
+	}
+	
+	private void buildGrid(File gridFile) throws FileNotFoundException {
+		String gridToText = "";
+		try (Scanner input = new Scanner(gridFile)) {
+            while (input.hasNextLine()) {
+                gridToText += input.nextLine() + "\n";
+            }
+        }
+		grid = new int[mazeSize][mazeSize];
+		String temp[] = gridToText.split("[\r]?\n");
+		for(int i = 0; i < mazeSize; i++) {
+			for(int j = 0; j < mazeSize; j++) {
+				if(temp[i].charAt(j) == '0')
+					grid[i][j] = 0;
+				else if(temp[i].charAt(j) == '1')
+					grid[i][j] = 1;
+			}
+		}
+		grid[start[0]][start[1]] = 2;
+		grid[goal[0]][goal[1]] = 3;
+	}
+	
+}
---
+++ Assignment1/src/GridDriver.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class GridDriver {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File problemFile = new File("src/resources/problem.txt");
+		int mazeSize = 0;
+		//The 0th index of start represents the x-coordinate, and the 1st index represents y-coordinate 
+		int[] start = new int[2];
+		//The 0th index of goal represents the x-coordinate, and the 1st index represents y-coordinate 
+		int[] goal = new int[2];
+		int algoIndex = 0;
+		int gridNumber = 0;
+		try (Scanner input = new Scanner(problemFile)) {
+			// Assuming the problem.txt has integers without any error
+			mazeSize = input.nextInt();
+			start[0] = input.nextInt();
+			start[1] = input.nextInt();
+			goal[0] = input.nextInt();
+			goal[1] = input.nextInt();
+			algoIndex = input.nextInt();
+			gridNumber = input.nextInt();
+        } catch (FileNotFoundException e) {
+        	System.out.println("Problem.txt does not exist");
+		}
+		
+		String gridFileName = "maze_"+gridNumber+".txt";
+		File gridFile = new File(gridFileName);
+		navigateGrid(gridFile, mazeSize, start, goal, algoIndex);
+	}
+	
+	private static void navigateGrid(File gridFile, int mazeSize, int[] start, int[] goal, int algoIndex) throws FileNotFoundException {
+		Grid problemGrid = new Grid(gridFile, mazeSize, start, goal);
+		// add a swith statement to call the appropriate algo
+	}
+
+}
