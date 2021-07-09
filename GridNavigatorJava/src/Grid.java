55a50775f2359ae2736816e414aeeca41b69876f
Rudra Kakadia <rkakadia@Rudras-MacBook-Air.local>
2021-07-09 12:00:46 +0530

    implemented display function. Checked inputting problem.txt and setup workflow

--- GridNavigatorJava/src/Grid.java
+++ GridNavigatorJava/src/Grid.java
@@ -13,6 +13,13 @@ public class Grid {
 	private int[] start;
 	private int[] goal;
 	private int mazeSize;
+	
+	private static final String ANSI_RESET = "\u001B[0m";
+	private static final String START = "\u001B[31m";
+	private static final String GOAL = "\u001B[32m";
+	private static final String PATH = "\u001B[33m";
+	private static final String BLOCK = "\u001B[35m";
+	public static final String ROAD = "\u001B[30m";
 
 	public Grid(File gridFile, int mazeSize, int[] start, int[] goal) throws FileNotFoundException {
 		this.mazeSize = mazeSize;
@@ -30,16 +37,18 @@ public class Grid {
         }
 		grid = new int[mazeSize][mazeSize];
 		String temp[] = gridToText.split("[\r]?\n");
-		for(int i = 0; i < mazeSize; i++) {
-			for(int j = 0; j < mazeSize; j++) {
-				if(temp[i].charAt(j) == '0')
-					grid[i][j] = 0;
-				else if(temp[i].charAt(j) == '1')
-					grid[i][j] = 1;
+		for(int i = 0; i < temp.length; i++) {
+			try	(Scanner scn = new Scanner(temp[i])) {
+				int x = scn.nextInt();
+				int y = scn.nextInt();
+				int val = scn.nextInt();
+				grid[x][y] = val;
 			}
 		}
 		grid[start[0]][start[1]] = 2;
+		System.out.println("Start is "+start[0]+", "+start[1]+" and grid num is "+grid[start[0]][start[1]]);
 		grid[goal[0]][goal[1]] = 3;
+		System.out.println("Goal is "+goal[0]+", "+goal[1]+" and grid num is "+grid[goal[0]][goal[1]]);
 	}
 
 	public void gridBFS() {
@@ -65,6 +74,31 @@ public class Grid {
 	public void gridOwnHeuristic() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void display() {
+		for(int i = 0; i < mazeSize; i++) {
+			for(int j = 0; j < mazeSize; j++) {
+				/*switch(grid[i][j]) {
+					case 1:
+						System.out.print(BLOCK + grid[i][j] + " " + ANSI_RESET);
+						break;
+					case 2:
+						System.out.print(START + grid[i][j] + " " + ANSI_RESET);
+						break;
+					case 3:
+						System.out.print(GOAL + grid[i][j] + " " + ANSI_RESET);
+						break;
+					case 4:
+						System.out.print(PATH + grid[i][j] + " " + ANSI_RESET);
+						break;
+					default:
+						System.out.print(ROAD + grid[i][j] + " " + ANSI_RESET); 
+				} */
+				System.out.print(grid[i][j] + " ");
+			}
+			System.out.println();
+		} 
 	}
 	
 }
--- GridNavigatorJava/src/GridDriver.java
+++ GridNavigatorJava/src/GridDriver.java
@@ -12,7 +12,7 @@ public class GridDriver {
 		//The 0th index of goal represents the x-coordinate, and the 1st index represents y-coordinate 
 		int[] goal = new int[2];
 		int algoIndex = 0;
-		int gridNumber = 0;
+		String gridNumber = "";
 		try (Scanner input = new Scanner(problemFile)) {
 			// Assuming the problem.txt has integers without any error
 			mazeSize = input.nextInt();
@@ -21,34 +21,43 @@ public class GridDriver {
 			goal[0] = input.nextInt();
 			goal[1] = input.nextInt();
 			algoIndex = input.nextInt();
-			gridNumber = input.nextInt();
+			gridNumber = input.next();
         } catch (FileNotFoundException e) {
         	System.out.println("Problem.txt does not exist");
 		}
 		
 		String gridFileName = "maze_"+gridNumber+".txt";
-		File gridFile = new File(gridFileName);
+		File gridFile = new File("src/resources/mazes/"+gridFileName);
 		navigateGrid(gridFile, mazeSize, start, goal, algoIndex);
+		
+		//Now time to run all the algorithms in 50 different environments
 	}
 	
 	private static void navigateGrid(File gridFile, int mazeSize, int[] start, int[] goal, int algoIndex) throws FileNotFoundException {
 		Grid problemGrid = new Grid(gridFile, mazeSize, start, goal);
-		// add a swith statement to call the appropriate algo
+		// add a switch statement to call the appropriate algo
 		switch(algoIndex) {
 			case 0:
 				problemGrid.gridBFS();
+				break;
 			case 1:
 				problemGrid.gridBidirectional();
+				break;
 			case 2: 
 				problemGrid.gridManhattan();
+				break;
 			case 3: 
 				problemGrid.gridHFive();
+				break;
 			case 4:
 				problemGrid.gridOwnHeuristic();
+				break;
 			default:
-				System.out.println("Invalid maze algorithm provided");
+				System.out.println("Invalid maze algorithm provided: "+algoIndex);
 				
 		}
+		
+		problemGrid.display();
 	}
 
 }
